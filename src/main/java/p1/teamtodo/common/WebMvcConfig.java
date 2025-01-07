fc6e79f65d459252ec843407bba84fa344e68557
unknown <wjddnr1gh@naver.com>
2025-01-07 09:19:20 +0900

    프론트 적용

--- src/main/java/p1/teamtodo/common/WebMvcConfig.java
+++ src/main/java/p1/teamtodo/common/WebMvcConfig.java
@@ -2,11 +2,16 @@ package p1.teamtodo.common;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.method.HandlerTypePredicate;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.io.IOException;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -21,6 +26,21 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/pic/**")
                 .addResourceLocations("file:" + uploadPath + "/");
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/**")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
+                        Resource resource = location.createRelative(resourcePath);
+
+                        if(resource.exists() && resource.isReadable()) {
+                            return resource;
+                        }
+
+                        return new ClassPathResource("/static/index.html");
+                    }
+                });
     }
 
     @Override
